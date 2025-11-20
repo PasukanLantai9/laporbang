37def4048524911d72b45a4d81105daff1011d5b
Naff <ahmadnafim30@gmail.com>
2025-11-20 13:39:10 +0700

    initial commit with splash screen

---
+++ app/src/main/java/com/example/laporbang/NavGraph.kt
@@ -0,0 +1,26 @@
+package com.example.laporbang
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.laporbang.AnimatedSplashScreen
+
+@Composable
+fun SetupNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Splash.route
+    ) {
+        composable(route = Screen.Splash.route) {
+            AnimatedSplashScreen(navController = navController)
+        }
+        composable(route = Screen.Home.route) {
+            HomeScreen()
+        }
+    }
+}
